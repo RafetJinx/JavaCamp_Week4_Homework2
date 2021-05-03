e80c3b0a23a459765d87f4c29ed6b1ba45069ee5
Rafet Ersoy <rafet.ersoy.cs@gmail.com>
2021-05-03 16:46:04 +0300

    JavaCamp_Week4_Homework2
    Engin Demiroğ - Yazılım Geliştirici Yetiştirme Kampı (JAVA & REACT) 4. hafta 2. ödev

---
+++ src/Concrete/CustomerCheckManager.java
@@ -0,0 +1,16 @@
+package Concrete;
+
+import java.rmi.RemoteException;
+
+import Abstract.CustomerCheckService;
+import Abstract.CustomerService;
+import Entities.Customer;
+import tr.gov.nvi.tckimlik.WS.*;
+
+public class CustomerCheckManager implements CustomerCheckService {
+
+    @Override
+    public boolean checkIfRealPerson(Customer customer) {
+    	return true;
+    }
+}
